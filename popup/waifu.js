d4de7a72ed2dca2bca4797cf97269026cee6cce9
TecnoCreeper <gregorix006@gmail.com>
2026-04-24 18:48:22 +0200

    Version 2.0.0 Fixed API changes.

--- popup/waifu.js
+++ popup/waifu.js
@@ -1,221 +1,229 @@
-// function to replace onClick event since it's blocked in webextension
-document.addEventListener('DOMContentLoaded', function () {
-    const generateButton = document.getElementById('new-waifu');
-    const infoButton = document.getElementById('info');
+// Replace onClick event since it's blocked in webextension
+document.addEventListener("DOMContentLoaded", function () {
+    const generateButton = document.getElementById("new-waifu");
+    const infoButton = document.getElementById("info");
     // onClick's logic below:
-    generateButton.addEventListener('click', function () {
+    generateButton.addEventListener("click", function () {
         getWaifu();
     });
-    infoButton.addEventListener('click', function () {
+    infoButton.addEventListener("click", function () {
         toggleInfo();
     });
 });
 
 // when popup is opened, load image and description from local storage
-restoreImage();
+restoreState();
 
 // generate new image and set description
 function getWaifu() {
-
     // set loading gif
     const imgTag = document.getElementById("waifu");
     imgTag.setAttribute("src", "Spin-1s-200px.gif");
 
-    // api get request
+    // api request
     const xhr = new XMLHttpRequest();
-    xhr.open("GET", "https://api.waifu.im/search/?is_nsfw=false");
+    xhr.open("GET", "https://api.waifu.im/images/?IsNsfw=False");
+    xhr.setRequestHeader("Accept-Version", "v7");
     xhr.send();
     xhr.onload = function () {
-
-        const response = JSON.parse(xhr.responseText);
-
-        // get wanted data from response
-        const url = response.images[0].url;
-        const source = response.images[0].source;
-        const is_nsfw = response.images[0].is_nsfw;
-
-        const tag_list = [];
-        response.images[0].tags.forEach(element => {
-            tag_list.push(element.name)
-        });
-        const tags = tag_list.join(", ");
-
-        const extension = response.images[0].extension;
-        const width = response.images[0].width;
-        const height = response.images[0].height;
-        const byte_size = response.images[0].byte_size;
-        const dominant_color = response.images[0].dominant_color;
-
-
-        // set description
-        const description = document.getElementById('description');
-        rawHTML = `<p class='text-break'>
-                                    Source:
-                                    <a href='${source}' target='_blank'>${getDomainName(source)}</a><br>
-                                    <br>
-                                    Is NSFW:
-                                    ${is_nsfw}<br>
-                                    <br>
-                                    Tags:
-                                    ${tags}<br>
-                                    <br>
-                                    Extension:
-                                    ${extension}<br>
-                                    <br>
-                                    Width:
-                                    ${width}<br>
-                                    <br>
-                                    Height:
-                                    ${height}<br>
-                                    <br>
-                                    Byte size:
-                                    ${byte_size}<br>
-                                    <br>
-                                    Dominant color:
-                                    ${dominant_color}
-                                </p>`;
-        let cleanHTML = DOMPurify.sanitize(rawHTML);
-        description.innerHTML = cleanHTML;
-
-        // set image
-        const img = new Image();
-        img.src = url;
-        img.onload = function () {
-            imgTag.src = img.src;
+        if (xhr.status !== 200) {
+            throw new Error();
         }
 
+        const response = JSON.parse(xhr.responseText).items[0];
+
+        let source = response.source;
+        // if it's a reddit url and it's broken it gets fixed
+        const shortlink = handleRedditUrl(response.source);
+        if (shortlink) {
+            source = shortlink;
+        }
+
+        const dominant_color = response.dominantColor;
+        const tags = response.tags.map((t) => t.name).join(", ");
+
+        let artist;
+        if (response.artists.length !== 0) {
+            artist = getArtistDetails(response);
+        }
+
+        // set description
+        const description = document.getElementById("description");
+        description.innerHTML = DOMPurify.sanitize(
+            crateInfoHTML(source, tags, dominant_color, artist),
+        );
+
+        // set image
+        imgTag.setAttribute("src", response.url);
+
+        const infovisibility = document
+            .getElementById("description-col")
+            .classList.contains("d-none")
+            ? "hide"
+            : "show";
+
         // save current state to local storage
-        saveState(url, source, is_nsfw, tags, extension, width, height, byte_size, dominant_color);
+        saveState(
+            response.url,
+            source,
+            tags,
+            dominant_color,
+            artist,
+            infovisibility,
+        );
+    };
+}
+
+function getArtistDetails(response) {
+    const artist = response.artists[0];
+
+    let link;
+    if (artist.twitter) {
+        link = artist.twitter;
+    } else if (artist.pixiv) {
+        link = artist.pixiv;
+    } else if (artist.patreon) {
+        link = artist.patreon;
+    } else if (artist.deviantArt) {
+        link = artist.deviantArt;
+    }
+
+    return { name: artist.name, link };
+}
+
+function handleRedditUrl(url) {
+    const regexp = /reddit\.com\/.{6}/i;
+    if (regexp.test(url)) {
+        const id = regexp.exec(url)[0].split("/")[1];
+        return `https://redd.it/${id}`;
     }
 }
 
-// info column toggle
-function toggleInfo() {
+function crateInfoHTML(source, tags, dominant_color, artist) {
+    let rawHTML = "<p class='text-break'>";
+    if (source) {
+        rawHTML += `
+                <a href='${source}' target='_blank'>Source</a><br /><br />
+            `;
+    }
 
-    // thing that change: min-width of 'root', info column visibility, content column width, info button color
+    rawHTML += `
+                Tags: ${tags}<br /><br />
+                Dominant color: ${dominant_color}
+        `;
 
-    const descriptionCol = document.getElementById('description-col');
-    const contentCol = document.getElementById('content-col');
-    const infoButton = document.getElementById('info');
-    const rootDiv = document.getElementById('root');
+    if (artist) {
+        if (artist.name && !artist.link) {
+            rawHTML += `
+                <br /><br />
+                Artist: ${artist.name}
+            `;
+        } else if (artist.name && artist.link) {
+            rawHTML += `
+                <br /><br />
+                Artist: <a href='${artist.link}' target='_blank'>${artist.name}</a>
+            `;
+        }
+    }
 
-    // if info is hidden, then show
-    if (descriptionCol.classList.contains("d-none")) {
+    rawHTML += "</p>";
 
-        // info ON state
+    return rawHTML;
+}
 
-        rootDiv.classList.add("expanded-minwidth")
+function toggleInfo(state) {
+    // things that change: min-width of 'root', info column visibility, content column width, info button color
 
-        descriptionCol.classList.remove("d-none");
+    const descriptionCol = document.getElementById("description-col");
+    const contentCol = document.getElementById("content-col");
+    const infoButton = document.getElementById("info");
+    const rootDiv = document.getElementById("root");
+
+    if (
+        (descriptionCol.classList.contains("d-none") && !state) ||
+        state === "show"
+    ) {
+        // switch to show info
+
+        rootDiv.classList.add("expanded-minwidth");
 
         contentCol.classList.remove("col-12");
         contentCol.classList.add("col-8");
 
+        descriptionCol.classList.remove("d-none");
+
         infoButton.classList.remove("btn-outline-info");
         infoButton.classList.add("btn-info");
-    }
 
-    // if info is shown, then hide
-    else {
-
-        // info OFF state
-
-        rootDiv.classList.remove("expanded-minwidth")
+        browser.storage.local.set({ infovisibility: "show" });
+    } else if (
+        (!descriptionCol.classList.contains("d-none") && !state) ||
+        state === "hide"
+    ) {
+        // switch to hide info
 
         descriptionCol.classList.add("d-none");
 
         contentCol.classList.remove("col-8");
         contentCol.classList.add("col-12");
 
+        rootDiv.classList.remove("expanded-minwidth");
+
         infoButton.classList.remove("btn-info");
         infoButton.classList.add("btn-outline-info");
+
+        browser.storage.local.set({ infovisibility: "hide" });
     }
 }
 
 // save current state to local storage
-function saveState(imageURL, imgSource, is_nsfw, tags, extension, width, height, byte_size, dominant_color) {
+function saveState(
+    imageURL,
+    imgSource,
+    tags,
+    dominant_color,
+    artist,
+    infovisibility,
+) {
     const data = {
-        "url": imageURL,
-        "source": imgSource,
-        "is_nsfw": is_nsfw,
-        "tags": tags,
-        "extension": extension,
-        "width": width,
-        "height": height,
-        "byte_size": byte_size,
-        "dominant_color": dominant_color
-    }
-    browser.storage.local.set(data)
+        url: imageURL,
+        source: imgSource,
+        tags: tags,
+        dominant_color: dominant_color,
+        artist: artist,
+        infovisibility: infovisibility,
+    };
+    browser.storage.local.set(data);
 }
 
 // when popup is opened, load image and description from local storage
-function restoreImage() {
+function restoreState() {
     browser.storage.local.get(null).then(function (data) {
-
         // if there is data in local storage
         if (data.url) {
-            // get data from local storage
-            const url = data.url;
-            const source = data.source;
-            const is_nsfw = data.is_nsfw;
-            const tags = data.tags;
-            const extension = data.extension;
-            const width = data.width;
-            const height = data.height;
-            const byte_size = data.byte_size;
-            const dominant_color = data.dominant_color;
-
             // set description
-            const description = document.getElementById('description');
-            rawHTML = `<p class='text-break'>
-                                        Source:
-                                        <a href='${source}' target='_blank'>${getDomainName(source)}</a><br>
-                                        <br>
-                                        Is NSFW:
-                                        ${is_nsfw}<br>
-                                        <br>
-                                        Tags:
-                                        ${tags}<br>
-                                        <br>
-                                        Extension:
-                                        ${extension}<br>
-                                        <br>
-                                        Width:
-                                        ${width}<br>
-                                        <br>
-                                        Height:
-                                        ${height}<br>
-                                        <br>
-                                        Byte size:
-                                        ${byte_size}<br>
-                                        <br>
-                                        Dominant color:
-                                        ${dominant_color}
-                                    </p>`;
-            let cleanHTML = DOMPurify.sanitize(rawHTML);
-            description.innerHTML = cleanHTML;
+            const description = document.getElementById("description");
+            description.innerHTML = DOMPurify.sanitize(
+                crateInfoHTML(
+                    data.source,
+                    data.tags,
+                    data.dominant_color,
+                    data.artist,
+                ),
+            );
+
+            // set info panel
+            toggleInfo(data.infovisibility);
 
             // set image
             const imgTag = document.getElementById("waifu");
-            const img = new Image();
-            img.src = url;
-            img.onload = function () {
-                imgTag.src = img.src;
-            }
-        }
-        // if there is no data in local storage
-        else {
+            imgTag.setAttribute("src", data.url);
+        } else {
             const imgTag = document.getElementById("waifu");
             imgTag.src = "";
-            const description = document.getElementById('description');
+            const description = document.getElementById("description");
             description.innerHTML = "<p class='text-break'>No description</p>";
-
+            toggleInfo("hide");
         }
     });
-}
-
-// extract the domain name from a url
-function getDomainName(url) {
-    const domain = url.replace('http://', '').replace('https://', '').replace('www.', '').split(/[/?#]/)[0];
-    return domain;
 }
